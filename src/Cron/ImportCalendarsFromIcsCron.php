76dba18092c6055a2b233d6e02d0e670c60c40c2
Thomas Körner <t.koerner@heimrich-hannot.de>
2024-04-01 11:37:54 +0200

    initialize contao framework on cron

--- src/Cron/ImportCalendarsFromIcsCron.php
+++ src/Cron/ImportCalendarsFromIcsCron.php
@@ -15,17 +15,21 @@ namespace Cgoit\ContaoCalendarIcalBundle\Cron;
 use Cgoit\ContaoCalendarIcalBundle\Import\IcsImport;
 use Contao\CalendarModel;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCronJob;
+use Contao\CoreBundle\Framework\ContaoFramework;
 
 #[AsCronJob('daily')]
 class ImportCalendarsFromIcsCron
 {
     public function __construct(
         private readonly IcsImport $icsImport,
+        private readonly ContaoFramework $contaoFramework,
     ) {
     }
 
     public function __invoke(): void
     {
+        $this->contaoFramework->initialize();
+
         $arrCalendars = CalendarModel::findBy(['ical_source != ?'], ['']);
 
         if (!empty($arrCalendars)) {
