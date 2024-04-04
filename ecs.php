7dcc6c658da36b37251eec5b11a5e304303c3b9f
cgoIT <info@cgo-it.de>
2024-04-04 13:42:58 +0200

    docs: update header in files

--- ecs.php
+++ ecs.php
@@ -9,7 +9,7 @@ return static function (ECSConfig $ecsConfig): void {
     $year = date('Y');
     $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
         'header' => <<<EOF
-This file is part of cgoit\\contao-calendar-ical-php8-bundle for Contao Open Source CMS.
+This file is part of cgoit\\contao-calendar-ical-bundle for Contao Open Source CMS.
 
 @copyright  Copyright (c) $year, cgoIT
 @author     cgoIT <https://cgo-it.de>
