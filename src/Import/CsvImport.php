c7f18ff315431a8b11ae43d178a8ed4688e53e45
Zalán Somogyváry <somogyvary@edvlab.tuwien.ac.at>
2026-02-06 13:23:50 +0100

    [Fix] Change copyrights to fix ecs.

--- src/Import/CsvImport.php
+++ src/Import/CsvImport.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of cgoit\contao-calendar-ical-bundle for Contao Open Source CMS.
+ *
+ * @copyright  Copyright (c) 2026, cgoIT
+ * @author     cgoIT <https://cgo-it.de>
+ * @license    LGPL-3.0-or-later
+ */
+
 namespace Cgoit\ContaoCalendarIcalBundle\Import;
 
 use Cgoit\ContaoCalendarIcalBundle\Import\Csv\CsvParser;
